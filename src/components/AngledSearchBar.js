b5273b950c530465d80408adced6eba3dce0be18
ex1 <skowko6750@naver.com>
2022-03-12 17:08:31 +0900

    ✨ 각진 검색창 디자인 구현 #23

---
+++ src/components/AngledSearchBar.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Box, Grid } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
+export const AngledSearchBar = () => {
+  return (
+    <Grid container justifyContent="center">
+      <Box
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          width: '960px',
+          maxWidth: '100%',
+        }}
+      >
+        <TextField
+          sx={{
+            '& fieldset': {
+              borderRadius: '0px',
+            },
+          }}
+          fullWidth
+          label="검색어를 입력하세요"
+          id="fullWidth"
+        />
+      </Box>
+    </Grid>
+  );
+};
+
+export default AngledSearchBar;
