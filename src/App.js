d7438b9d24224a8ad85afccdeed4c6c3dbe7e706
ex1 <skowko6750@naver.com>
2022-03-13 16:26:51 +0900

    ✨ 컴포넌트디자인테스트 페이지 생성 #32

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import MainPage from './pages/MainPage';
 import SearchPage from './pages/SearchPage';
+import DesignTestPage from './pages/DesignTestPage/DesignTestPage';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Route path="/" exact={true} component={MainPage} /> //라우팅 설정
+      <Route path="/" exact={true} component={MainPage} />
       <Route path="/search" exact={true} component={SearchPage} />
+      <Route path="/test" exact={true} component={DesignTestPage} />
     </div>
   );
 }
