69693729f2b107df725b623aa2afa2bb0530021a
ex1 <skowko6750@naver.com>
2022-03-15 00:24:39 +0900

    🐛 cors에러방지 설정 #36

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import axios from 'axios';
 import MainPage from './pages/MainPage';
 import SearchPage from './pages/SearchPage';
 import DesignTestPage from './pages/DesignTestPage/DesignTestPage';
@@ -8,6 +9,20 @@ import { Link, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <button
+        onClick={() => {
+          axios
+            .get('/api/campaigns')
+            .then((result) => {
+              console.log(result.data);
+            })
+            .catch(() => {
+              console.log('연결실패');
+            });
+        }}
+      >
+        axios 확인 버튼(콘솔로그 확인)
+      </button>
       <Route path="/" exact={true} component={MainPage} />
       <Route path="/search" exact={true} component={SearchPage} />
       <Route path="/test" exact={true} component={DesignTestPage} />
