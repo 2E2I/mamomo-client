deb33e34d3d821d32baae82a55b61f0cd3a8f28a
bobaejin <bo_bae_jin@naver.com>
2022-03-20 18:16:39 +0900

    :sparkles: 모든카테고리 태그 설계 #22
    Tag.js, TagList.js, CategoryTag.js 파일 생성 및 태그 디자인 수정(Tag.js)

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -3,14 +3,18 @@ import TopAppBar from '../../components/TopAppBar';
 import RoundSearchBar from '../../components/RoundSearchBar';
 import TagTitle from '../../components/searchPageComponents/SearchPageTagTitle';
 import Tags from '../../components/searchPageComponents/SearchPageTags';
+import Tag from '../../components/searchPageComponents/Tag';
 
 const SearchPage = () => {
     return (
         <div>
             <TopAppBar />
             <RoundSearchBar />
+            <hr/>old tag design
             <TagTitle />
             <Tags />
+            <hr/>new tag design
+            <Tag />
         </div>
     )
 };
