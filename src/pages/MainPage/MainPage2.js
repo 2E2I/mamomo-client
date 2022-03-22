a007a362baa10b93d751c7e612b092a7e9e474c7
ex1 <skowko6750@naver.com>
2022-03-22 17:16:24 +0900

    ✨ 디자인 비교를 위한 테스트 페이지 생성 #67

---
+++ src/pages/MainPage/MainPage2.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
+import TopAppBar from '../../components/TopAppBar';
+import RoundSearchBar from '../../components/mainPageComponents/MainPageSearchBar';
+import CardList2 from '../../components/mainPageComponents/MainPageDonationList';
+import Card from '../../components/OldDonationCard';
+import MainCarousel from '../../components/mainPageComponents/Carousel2';
+import Categories from '../../components/Categories';
+
+const MainPage2 = () => {
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        //minWidth: { xs: '100%', md: '1200px' },
+        //maxWidth: { xs: '100%', md: '2560px' },
+        width: '1000px',
+      }}
+      disableGutters={true}
+      justify="center"
+    >
+      <TopAppBar />
+      <MainCarousel />
+      <br />
+
+      <br />
+      <Categories />
+      <br />
+
+      <CardList2 />
+      <hr />
+    </Container>
+  );
+};
+
+export default MainPage2;
