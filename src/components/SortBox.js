7910f314341a541d78289e2d94fc90d3b3d5c921
ex1 <skowko6750@naver.com>
2022-04-09 16:03:33 +0900

    ✨ 카테고리 페이지 정렬기능 추가 #94

--- src/components/SortBox.js
+++ src/components/SortBox.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, styled } from '@mui/material';
 
-const sortBox = () => {
-    return (
-      <InnerOrganizationTitleBox>
-        <SortButton>추천순</SortButton>
-        <SortButton>최신순</SortButton>
-        <SortButton>마감순</SortButton>
-      </InnerOrganizationTitleBox>
-    );
+import { CategoryStore } from '../store/CategoryPageStore';
+import { makeStyles } from '@mui/styles';
+import { color } from '@mui/system';
+
+
+const color1 = (colorN, index) => {
+  if (colorN == index) return '#424242';
+  else return '#bdbdbd';
 };
 
-export default sortBox;
+const SortBox = () => {
+  const { setSortValue, sortIndex, setSortIndex } = CategoryStore(); //zustand
+  const [index, setIndex] = useState(1);
+  
+
+  return (
+    <InnerOrganizationTitleBox>
+      <SortButton
+        colorN={3}
+        onClick={() => {
+          setSortValue('sort=due_date,ASC');
+          setSortIndex(3);
+        }}
+        sx={{ color: color1(3, sortIndex) }}
+      >
+        마감순
+      </SortButton>
+      <SortButton
+        colorN={2}
+        onClick={() => {
+          setSortValue('sort=start_date,DESC');
+          setSortIndex(2);
+        }}
+        sx={{ color: color1(2, sortIndex) }}
+      >
+        최신순
+      </SortButton>
+      <SortButton
+        colorN={1}
+        onClick={() => {
+          setSortValue('heart=true');
+          setSortIndex(1);
+        }}
+        sx={{ color: color1(1, sortIndex) }}
+      >
+        추천순
+      </SortButton>
+    </InnerOrganizationTitleBox>
+  );
+};
+
+export default SortBox;
+
 
 const InnerOrganizationTitleBox = styled(Box)(() => ({
   direction: 'rtl',
@@ -23,13 +65,12 @@ const InnerOrganizationTitleBox = styled(Box)(() => ({
   flexDirection: 'row',
 }));
 
-const SortButton = styled(Box)(() => ({
+const SortButton = styled(Box)((colorN) => ({
   fontWeight: 700,
   fontSize: 18,
   fontFamily: 'Noto Sans KR',
   textOverflow: 'ellipsis',
   overflow: 'hidden',
-  color: '#bdbdbd',
   marginRight: '20px',
   '&:hover': {
     cursor: 'pointer',
