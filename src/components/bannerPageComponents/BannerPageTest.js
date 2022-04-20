1d7198a71cf3db221a415a7055cfa7cd604dfd66
ex1 <skowko6750@naver.com>
2022-04-20 23:52:39 +0900

    💄 배너페이지 로고 컴포넌트 디자인 #108

---
+++ src/components/bannerPageComponents/BannerPageTest.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Box, styled } from '@mui/material';
+import spb from '../../assets/images/sak.jpg';
+
+const BannerPageTest = () => {
+  return (
+    <InnerOrganizationTitleBox textAlign="center">
+      <SortButton>배너 제작</SortButton>
+    </InnerOrganizationTitleBox>
+  );
+};
+
+export default BannerPageTest;
+
+const InnerOrganizationTitleBox = styled(Box)(() => ({
+  fontFamily: 'Noto Sans KR',
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignContent: 'center',
+  backgroundColor: '#f9fbe7',
+  backgroundImage: `url(${spb})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  alignItems: 'center',
+  height: 100,
+  borderRadius: 10,
+  marginBottom: 10,
+}));
+
+const SortButton = styled(Box)(() => ({
+  fontWeight: 600,
+  fontSize: 28,
+  fontFamily: 'Noto Sans KR',
+  textOverflow: 'ellipsis',
+  alignItems: 'center',
+  overflow: 'hidden',
+  color: '#fafafa',
+  marginLeft: 700,
+}));
