8510ac46839176616395112910919bb058389e6c
bobaejin <bo_bae_jin@naver.com>
2022-04-24 22:58:07 +0900

    :sparkles: 로그인페이지 설계 #112
    유효성 검사 추가

--- src/components/signInPageComponents/SignIn.js
+++ src/components/signInPageComponents/SignIn.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Grid,
   Button,
   createTheme,
-  ThemeProvider
+  ThemeProvider,
+  Box
 } from '@mui/material';
 
 import axios from 'axios';
 import { SignInStore } from '../../store/SignInPageStore';
-import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom"; // <Route>로 감싸지지 않은 컴포넌트에서 history 사용
 
 // 로그인 버튼
 const SignIn = () => {
@@ -20,9 +21,10 @@ const SignIn = () => {
       },
     },
   });
-
+  
   const { email, password, keepingSignIn } = SignInStore();
   const history = useHistory();
+  const [error, setError] = useState(false);
 
   const onClick = async () => {
     try {
@@ -34,6 +36,7 @@ const SignIn = () => {
         })
         .then((res) => {
           console.log(res.data) // 토큰
+          setError(false);
           if (res.status === 200) {
             console.log('로그인 성공')
             history.push('/')
@@ -41,32 +44,45 @@ const SignIn = () => {
         })
     } catch (e) {
       console.log(e);
+      setError(true);
     }
   };
 
   return (
     <ThemeProvider theme={theme}>
       <Grid container justifyContent="center">
-       <Button
-        variant="contained"
-        color="main"
-        sx={{
-          m: "15px 0 0 0",
-          width: "400px",
-          height: "55px",
-          boxShadow: '0',
-          ":hover": {
-            boxShadow: '0'
-          },
-          fontFamily: 'Noto Sans KR',
-          fontSize: "16px",
-          fontWeight: 400,
-        }}
-        onClick={ onClick }
-      >
+        <Button
+          variant="contained"
+          color="main"
+          sx={{
+            m: "15px 0 0 0",
+            width: "400px",
+            height: "55px",
+            boxShadow: '0',
+            ":hover": {
+              boxShadow: '0'
+            },
+            fontFamily: 'Noto Sans KR',
+            fontSize: "16px",
+            fontWeight: 400,
+          }}
+          onClick={ onClick }
+        >
           로그인
         </Button>
       </Grid>
+      {
+        error === true
+        ? <Grid
+            container justifyContent="center"
+            margin="10px 0 0 0"
+            fontSize="12px"
+            color="#ff0000"
+          >
+            이메일 또는 비밀번호를 확인해주세요
+          </Grid>
+        : null
+      }
     </ThemeProvider>
     
   );
