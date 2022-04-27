935f70c116ab92f4e303b6a049b3b56bd4e0fb4d
bobaejin <bo_bae_jin@naver.com>
2022-04-28 01:58:30 +0900

    :sparkles: 닉네임 유효성 검사

--- src/components/signUpPageComponents/Nickname.js
+++ src/components/signUpPageComponents/Nickname.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Grid,
   TextField,
@@ -21,25 +21,60 @@ const Nickname = () => {
   });
 
   const { nickname, setNickname } = SignUpStore();
+  const [msg, setMsg] = useState('');
+  const [isNickname, setIsNickname] = useState('');
+
+  function Validation() {
+    if (nickname !== "") {
+      setIsNickname(true);
+    }
+    else if (nickname === "") {
+      setMsg("닉네임을 입력하세요.");
+      setIsNickname(false);
+    }
+  }
+
+  useEffect(() => {
+    Validation();
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container justifyContent="center">
-        <TextField
-          label="닉네임"
-          variant="outlined"
-          placeholder="특수문자 제외 2~10자"
-          color="gray"
-          sx={{
-            margin: "30px 0 0 0",
-            width: "400px",
-          }}
-          onChange={ (e) => {
-            setNickname(e.target.value)
-          }}
-        >
-        </TextField>
+      <Grid
+        sx={{
+          margin: "30px 0 0 0",
+        }}
+      >
+        <Grid container justifyContent="center">
+          <TextField
+            label="닉네임"
+            variant="outlined"
+            placeholder="특수문자 제외 2~10자"
+            color="gray"
+            sx={{
+              width: "400px"
+            }}
+            onChange={ (e) => {
+              setNickname(e.target.value)
+            }}
+          >
+          </TextField>
+        </Grid>
+        {
+          isNickname === true
+          ? null
+          :
+            <Grid span
+            margin="10px 0 0 0"
+            fontSize="12px"
+            color="#ff0000"
+            >
+              {msg}
+            </Grid>
+        }
+
       </Grid>
+      
     </ThemeProvider>
   );
 }
