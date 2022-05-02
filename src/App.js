50310763504e2b0379242d13416b1c05dd755ee9
bobaejin <bo_bae_jin@naver.com>
2022-05-02 23:24:15 +0900

    :sparkles: 마이페이지 라우팅
    App.js 충돌나서 추가 머지 !

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import SignUpPage from './pages/SignUpPage/SignUpPage';
 import MakeBannerPage from './pages/MakeBannerPage/MakeBannerPage';
 import SignInPage from './pages/SignInPage';
 import BannerListPage from './pages/BannerListPage/BannerListPage';
+import MyPage from './pages/MyPage/MyPage';
 
 import { authHeader, logout, getCurrentUser } from '../src/components/authenticationFunc';
 import { SignInStore } from '../src/store/SignInPageStore'
@@ -140,6 +141,7 @@ function App() {
         <Route path="/signup" exact={true} component={SignUpPage} />
         <Route path="/banner" exact={true} component={MakeBannerPage} />
         <Route path="/bannerList" exact={true} component={BannerListPage} />
+        <Route path="/my" exact={true} component={MyPage} />
       </BrowserRouter>
     </>
   );
