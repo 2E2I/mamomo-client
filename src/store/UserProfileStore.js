a13dc7cc15acb669b0f27e53ae1ea91d315a7438
bobaejin <bo_bae_jin@naver.com>
2022-05-05 17:58:18 +0900

    :sparkles: UserProfileStore 영속성 추가
    새로고침 되면 데이터 날라감

--- src/store/UserProfileStore.js
+++ src/store/UserProfileStore.js
@@ -1,25 +1,37 @@
 import create from 'zustand';
+import persist from '../utils/persist';
 
-const UserProfileStore = create((set) => ({
-  nickname: "",
-  setNickname(nickname) {
-    set(() => ({ nickname: nickname }));
-  },
+const UserProfileStore = create(
+  persist(
+    {
+      key: "User",
+    },
+    (set) => ({
+      // 사용자 닉네임
+      nickname: "",
+      setNickname(nickname) {
+        set(() => ({ nickname: nickname }));
+      },
 
-  sex: "",
-  setSex(sex) {
-    set(() => ({ sex: sex }));
-  },
+      // 사용자 성별
+      sex: "",
+      setSex(sex) {
+        set(() => ({ sex: sex }));
+      },
 
-  birth: "",
-  setBirth(birth) {
-    set(() => ({ birth: birth }));
-  },
+      // 사용자 생년월일
+      birth: "",
+      setBirth(birth) {
+        set(() => ({ birth: birth }));
+      },
 
-  favTopics: [],
-  setFavTopics(favTopics) {
-    set(() => ({ favTopics: favTopics }));
-  },
-}));
+      // 사용자 관심 기부 분야
+      favTopics: [],
+      setFavTopics(favTopics) {
+        set(() => ({ favTopics: favTopics }));
+      },
+    }),
+  ),
+);
 
 export { UserProfileStore };
