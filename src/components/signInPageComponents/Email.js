22df205ca70eda6bdbbff0df79b157ca13af4118
bobaejin <bo_bae_jin@naver.com>
2022-05-29 23:51:27 +0900

    :zap: 로그인 엔터키 기능 추가 #221

--- src/components/signInPageComponents/Email.js
+++ src/components/signInPageComponents/Email.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   Grid,
   TextField,
   createTheme,
   ThemeProvider,
-  Box
 } from '@mui/material';
 
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
 import { SignInStore } from '../../store/SignInPageStore';
-import { SignUpStore } from '../../store/SignUpPageStore';
+import { UserProfileStore } from '../../store/UserProfileStore';
 
 // email 입력란
 const Email = () => {
@@ -21,16 +23,50 @@ const Email = () => {
     },
   });
 
-  const { email, setEmail } = SignInStore();
-  const { setPassword, setCheckingPassword, setNickname, setSex, setBirth} = SignUpStore();
+  const { email, password, setEmail, setStatus, error, setError } = SignInStore();
+  const { setImg, setNickname, setSex, setBirthday, setFavTopics } = UserProfileStore();
+  const history = useHistory();
 
-  useEffect(() => {
-    setPassword('');
-    setCheckingPassword('');
-    setNickname('');
-    setSex('');
-    setBirth('');
-  }, []);
+  const userFavTopics = [];
+
+  const onClick = async () => {
+    await axios
+      .post("http://localhost:8080/api/user/authenticate", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        setError(false);
+        setStatus(true);
+
+        console.log(res.data.profile.birth);
+
+        for (var i = 0; i < (Object.values(res.data.profile.favTopics).length); i++) {
+          userFavTopics.push(Object.values(res.data.profile.favTopics)[i].topic.id)
+        }
+
+        if (res.status === 200) {
+          localStorage.setItem('user', res.data.token);
+          setImg(res.data.profile.profileImgUrl);
+          setNickname(res.data.profile.nickname);
+          setBirthday(res.data.profile.birth);
+          setSex(res.data.profile.sex);
+          setFavTopics(userFavTopics);
+          console.log('로그인 성공');
+          history.push('/');
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(true);
+        setStatus(false);
+      });
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter')
+      onClick();
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -47,6 +83,7 @@ const Email = () => {
           onChange={ (e) => {
             setEmail(e.target.value)
           }}
+          onKeyDown= { onKeyPress }
         >
         </TextField>
       </Grid>
